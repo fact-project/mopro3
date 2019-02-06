4c6b1035a5e9d7b6e25da6d3b138bfe405bfc261
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-02-06 20:09:02 +0100

    Create only jobs that do not exist yet

--- examples/add_ceres_runs.py
+++ examples/add_ceres_runs.py
@@ -6,13 +6,25 @@ from mopro.database import (
     CeresRun,
     Status,
 )
+from peewee import JOIN
 
 initialize_database()
 
 with database.connection_context():
-    for corsika_run in CorsikaRun.select(CorsikaRun.id):
+    ceres_settings = CeresSettings.select(CeresSettings.id).limit(1)
+    corsika_runs = (
+        CorsikaRun
+        .select(CorsikaRun.id)
+        .join(CeresRun, JOIN.LEFT_OUTER)
+        .join(CeresSettings, JOIN.LEFT_OUTER)
+        .where((CeresRun.id == None) | (CeresSettings.id != ceres_settings))
+        .distinct()
+    )
+    print(corsika_runs.count())
+    for corsika_run in corsika_runs:
+        print(corsika_run.id)
         print('New job with id', CeresRun.insert(
-            ceres_settings=CeresSettings.select(CeresSettings.id).limit(1),
+            ceres_settings=ceres_settings,
             corsika_run=corsika_run,
             status=Status.select(Status.id).where(Status.name == 'created'),
         ).execute(), 'inserted')
