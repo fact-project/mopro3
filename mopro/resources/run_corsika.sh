defdfbe2a6a6958bd23364dec94c845448032910
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-02-07 11:36:44 +0100

    More debug output in run_corsika.sh

--- mopro/resources/run_corsika.sh
+++ mopro/resources/run_corsika.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+echo "python executable"
 which python
+
+echo -e "\n\npython version"
 python --version
+
+echo -e "\n\nMOPRO env variables"
 env | grep MOPRO
+
+echo -e "\n\nAvailable space in /tmp"
+df -h /tmp
+
+echo -e "\n\nStart processing"
 python -m mopro.processing.run_corsika
