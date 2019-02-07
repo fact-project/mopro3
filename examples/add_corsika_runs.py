0ef80006dba63876bbfdf7e8e87c5530884316b2
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-02-07 17:41:32 +0100

    Respect maximum sql variables for sqlite

--- examples/add_corsika_runs.py
+++ examples/add_corsika_runs.py
@@ -9,7 +9,9 @@ from mopro.database import (
 initialize_database()
 
 
-runs_per_bin = 1
+runs_per_bin = 10
+delta_zd = 5
+delta_az = 10
 
 corsika_settings = (
     CorsikaSettings
@@ -20,7 +22,8 @@ corsika_settings = (
 
 options = dict(
     primary_particle=14,
-    n_showers=100,
+    n_showers=1000,
+    walltime=120,
     energy_min=100,
     energy_max=200e3,
     spectral_index=-2.7,
@@ -33,21 +36,19 @@ options = dict(
 
 
 def generator():
-    delta_zd = 5
-    delta_az = 10
+    for min_az in range(0, 360, delta_az):
+        yield dict(
+            zenith_min=min_zd,
+            zenith_max=min_zd + delta_zd,
+            azimuth_min=min_az,
+            azimuth_max=min_az + delta_az,
+            **options,
+        )
+
+
+for i in range(runs_per_bin):
     for min_zd in range(0, 30, delta_zd):
-        for min_az in range(0, 360, delta_az):
-            for i in range(runs_per_bin):
-                yield dict(
-                    zenith_min=min_zd,
-                    zenith_max=min_zd + delta_zd,
-                    azimuth_min=min_az,
-                    azimuth_max=min_az + delta_az,
-                    **options,
-                )
-
-
-with database.connection_context():
-    print(CorsikaRun.insert_many(
-        generator(),
-    ).execute())
+        with database.connection_context():
+            print(CorsikaRun.insert_many(
+                generator(),
+            ).execute())
