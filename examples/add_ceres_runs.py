b00cd45ceab0975c943393a69ae5beec3cd5149c
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-02-08 16:23:49 +0100

    Use insert_many for ceres runs

--- examples/add_ceres_runs.py
+++ examples/add_ceres_runs.py
@@ -12,21 +12,25 @@ initialize_database()
 
 with database.connection_context():
     ceres_settings = CeresSettings.select(CeresSettings.id).limit(1)
-    corsika_runs = (
+
+    corsika_runs = list(
         CorsikaRun
-        .select(CorsikaRun.id)
+        .select(CorsikaRun.id).alias('corsika_run_id')
         .join(CeresRun, JOIN.LEFT_OUTER)
         .join(CeresSettings, JOIN.LEFT_OUTER)
         .where((CeresRun.id == None) | (CeresSettings.id != ceres_settings))
         .distinct()
     )
-    print(corsika_runs.count())
-    for corsika_run in corsika_runs:
-        print(corsika_run.id)
-        print('New job with id', CeresRun.insert(
+
+    print('CORSIKA without corresponding ceres run:', len(corsika_runs))
+    print(corsika_runs[0])
+
+    print(CeresRun.insert_many((
+        dict(
+            corsika_run_id=corsika_run.id,
             ceres_settings=ceres_settings,
-            corsika_run=corsika_run,
             status=Status.select(Status.id).where(Status.name == 'created'),
             off_target_distance=6,
             diffuse=True,
-        ).execute(), 'inserted')
+        ) for corsika_run in corsika_runs),
+    ).execute())
