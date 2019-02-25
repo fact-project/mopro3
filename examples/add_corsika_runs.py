a951a0ed92ab02aebdf443fb532be6f6da21fa78
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2019-02-25 12:45:56 +0100

    Update example

--- examples/add_corsika_runs.py
+++ examples/add_corsika_runs.py
@@ -9,21 +9,21 @@ from mopro.database import (
 initialize_database()
 
 
-runs_per_bin = 10
+runs_per_bin = 20
 delta_zd = 5
 delta_az = 10
 
 corsika_settings = (
     CorsikaSettings
     .select(CorsikaSettings.id)
-    .where(CorsikaSettings.name == 'epos_fluka_iact_lapalma_winter')
+    .where(CorsikaSettings.name == 'epos_urqmd_iact_lapalma_winter')
     .where(CorsikaSettings.version == 76900)
 )
 
 options = dict(
     primary_particle=14,
-    n_showers=1000,
-    walltime=120,
+    n_showers=10000,
+    walltime=2880,
     energy_min=100,
     energy_max=200e3,
     spectral_index=-2.7,
@@ -36,19 +36,19 @@ options = dict(
 
 
 def generator():
-    for min_az in range(0, 360, delta_az):
-        yield dict(
-            zenith_min=min_zd,
-            zenith_max=min_zd + delta_zd,
-            azimuth_min=min_az,
-            azimuth_max=min_az + delta_az,
-            **options,
-        )
+    for i in range(runs_per_bin):
+        for min_zd in range(0, 30, delta_zd):
+            for min_az in range(0, 360, delta_az):
+                yield dict(
+                    zenith_min=min_zd,
+                    zenith_max=min_zd + delta_zd,
+                    azimuth_min=min_az,
+                    azimuth_max=min_az + delta_az,
+                    **options,
+                )
 
 
-for i in range(runs_per_bin):
-    for min_zd in range(0, 30, delta_zd):
-        with database.connection_context():
-            print(CorsikaRun.insert_many(
-                generator(),
-            ).execute())
+with database.connection_context():
+    print(CorsikaRun.insert_many(
+        generator(),
+    ).execute())
